a14385a3ae2e6596a7d0a3305d4c106408a2ac4c
lvning <lvning@datamesh.com>
2017-08-01 10:13:32 +0800

    change plugin name.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // 使用jquery来调用handlerbars 渲染页面内容
 (function($) {
     var compiled = {};
-    $.fn.handlebars = function(data) {
+    $.fn.render = function(data) {
       var template = this.html()
       compiled[template] = Handlebars.compile(template);
       this.after(compiled[template](data));
